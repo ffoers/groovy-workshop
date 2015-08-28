b8fa58ec8301e71b490a7c2326d71b33dc03631c
FAbian Förster <githubprojects@ffoerster.com>
2015-08-28 09:35:32 +0200

    DSL exercise added

--- src/exercises/five.groovy
+++ src/exercises/five.groovy
@@ -1,1 +1,5 @@
 // JSON handling
+
+// TODO create a few classes / objects and output them as JSON
+
+// TODO parse the same JSON
--- src/exercises/six.groovy
+++ src/exercises/six.groovy
@@ -1,1 +1,37 @@
+import groovy.transform.CompileStatic
+import groovy.transform.ToString
+
 //Delegate Handling
+
+@CompileStatic
+@ToString
+class Stuff {
+    Integer integer
+
+    def integer(Integer i) {
+        this.integer = i
+    }
+}
+
+@CompileStatic
+Stuff bla(Closure dsl) {
+    def p = new Stuff() {}
+    def code = dsl.rehydrate(p, this, this)
+    code.delegate = p
+    code()
+    return p
+}
+
+Stuff createStuff() {
+    Stuff stuff = bla {
+//        word 'k',
+        integer 2
+    }
+
+    assert stuff.integer == 2
+    stuff
+}
+
+println createStuff()
+
+//TODO: Create a DSL that has sensible defaults but can configure a server with hostname, port, useTls
